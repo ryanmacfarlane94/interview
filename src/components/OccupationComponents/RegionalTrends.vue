085cf20b3edfbcb835f93db39f4e9994d5227dec
Ryan MacFarlane <ryan.macfarlane@codejamconsulting.com>
2021-06-20 22:25:52 -0700

    Add Chart

--- src/components/OccupationComponents/RegionalTrends.vue
+++ src/components/OccupationComponents/RegionalTrends.vue
@@ -1,6 +1,7 @@
 <template>
     <h2><strong>Regional Trends</strong></h2>
     <div class="divider"></div>
+    <div id="chart" class="chart"></div>
     <div class="regional-trends-container">
       <table>
         <tr>
@@ -11,21 +12,21 @@
             <th class="col-md">% Change</th>
         </tr>
         <tr>
-            <td class="col-md-8">Region</td>
+            <td class="col-md-8"><img src="/img/circle.JPG" class="point"/>&emsp;Region</td>
             <td class="col-md">{{ trendComparison.regional[0].toLocaleString() }}</td>
             <td class="col-md">{{ trendComparison.regional[trendComparison.regional.length - 1].toLocaleString() }}</td>
             <td class="col-md">{{ (trendComparison.regional[trendComparison.regional.length - 1] - trendComparison.regional[0]).toLocaleString() }}</td>
             <td class="col-md">{{ getChange(trendComparison.regional) }}%</td>
         </tr>
         <tr>
-            <td class="col-md-8">State</td>
+            <td class="col-md-8"><img src="/img/square.JPG" class="point"/>&emsp;State</td>
             <td class="col-md">{{ trendComparison.state[0].toLocaleString() }}</td>
             <td class="col-md">{{ trendComparison.state[trendComparison.state.length - 1].toLocaleString() }}</td>
             <td class="col-md">{{ (trendComparison.state[trendComparison.state.length - 1] - trendComparison.state[0]).toLocaleString() }}</td>
             <td class="col-md">{{ getChange(trendComparison.state) }}%</td>
         </tr>
         <tr>
-            <td class="col-md-8">Nation</td>
+            <td class="col-md-8"><img src="/img/triangle.JPG" class="point"/>&emsp;Nation</td>
             <td class="col-md">{{ trendComparison.nation[0].toLocaleString() }}</td>
             <td class="col-md">{{ trendComparison.nation[trendComparison.nation.length - 1].toLocaleString() }}</td>
             <td class="col-md">{{ (trendComparison.nation[trendComparison.nation.length - 1] - trendComparison.nation[0]).toLocaleString() }}</td>
@@ -38,28 +39,56 @@
 <script>
 
 import '../../assets/styles/OccupationComponents/regional-trends.css';
+import {GoogleCharts} from 'google-charts';
+
 
 export default {
   name: 'RegionalTrends',
   props: {
       trendComparison: Object
   },
-  data: function () {
-    return {
-
-    }
-  },
   mounted: function () {
-
+    GoogleCharts.load(this.drawChart);
   },
   methods: {
-      getPercentChange() {
+    getChange(data) {
+        var change = data[data.length - 1] - data[0];
+        return (change / data[0] * 100).toFixed(1).toLocaleString();
+    },
+    drawChart() {
+        const data = GoogleCharts.api.visualization.arrayToDataTable(this.getData());
+        const chart = new GoogleCharts.api.visualization.LineChart(document.getElementById('chart'));
+        var options = {
+          legend: { position: 'bottom' },
+          vAxis: {
+            title: 'Percentage Change'
+          },
+          pointSize: 5,
+          series: {
+            0: { color: '#001145', pointShape: 'circle' },
+            1: { color: '#14ACDE', pointShape: 'square'},
+            2: { color: '#95E5FF', pointShape: 'triangle' }
+          }
+        };
 
-      },
-      getChange(data) {
-          var change = data[data.length - 1] - data[0];
-          return (change / data[0] * 100).toFixed(1).toLocaleString();
-      }
+        chart.draw(data, options);
+    },
+    getData() {
+        var data = [
+            ['Year','Region','State','Nation'],
+            [this.trendComparison.start_year.toString(),0,0,0]
+        ];
+        var i;
+        var year = this.trendComparison.start_year + 1;
+        for (i = 1;i < this.trendComparison.regional.length;i++) {
+            var regionChange = (this.trendComparison.regional[i] - this.trendComparison.regional[0]) / this.trendComparison.regional[0] * 100;
+            var stateChange = (this.trendComparison.state[i] - this.trendComparison.state[0]) / this.trendComparison.state[0] * 100;
+            var nationChange = (this.trendComparison.nation[i] - this.trendComparison.nation[0]) / this.trendComparison.nation[0] * 100;
+            data.push([year.toString(), regionChange, stateChange, nationChange]);
+            year++;
+        }
+        return data;
+    }
   }
 }
 </script>
--- src/components/OccupationOverview.vue
+++ src/components/OccupationOverview.vue
@@ -19,11 +19,11 @@
 </template>
 
 <script>
+
 import Summary from './OccupationComponents/Summary.vue'
 import RelatedIndustries from './OccupationComponents/RelatedIndustries.vue'
 import RegionalTrends from './OccupationComponents/RegionalTrends.vue'
-
-//import axios from 'axios';
+import axios from 'axios';
 
 
 export default {
@@ -41,7 +41,7 @@ export default {
       summary:null,
       jobsGrowth:null,
       employingIndustries:null,
-      trendComparison:null
+      trendComparison:null,
     }
   },
   mounted: function () {
@@ -50,98 +50,9 @@ export default {
   },
   methods: {
     getOccupationData() {
-        var response = {};
-        response.data = {
-  occupation: {
-    onet: "15-1131",
-    title: "Computer Programmers"
-  },
-  region: {
-    type: "MSA",
-    id: "42660",
-    title: "Seattle-Tacoma-Bellevue, WA"
-  },
-  summary: {
-    jobs: {
-      year: 2015,
-      regional: 12352,
-      national_avg: 6501
-    },
-    jobs_growth: {
-      start_year: 2013,
-      end_year: 2018,
-      regional: 10.2,
-      national_avg: 8.5
-    },
-    earnings: {
-      regional: 57.24,
-      national_avg: 38.2
-    }
-  },
-  trend_comparison: {
-    start_year: 2013,
-    end_year: 2018,
-    regional: [
-      11904,
-      12384,
-      12352,
-      12680,
-      12920,
-      13114
-    ],
-    state: [
-      13103,
-      13598,
-      13599,
-      13968,
-      14244,
-      14469
-    ],
-    nation: [
-      300651,
-      307024,
-      314154,
-      318998,
-      326205
-    ]
-  },
-  employing_industries: {
-    year: 2015,
-    jobs: 12352,
-    industries: [
-      {
-        naics: "511210",
-        title: "Software Publishers",
-        in_occupation_jobs: 4654,
-        jobs: 52886
-      },
-      {
-        naics: "541512",
-        title: "Computer Systems Design Services",
-        in_occupation_jobs: 1873,
-        jobs: 20582
-      },
-      {
-        naics: "541512",
-        title: "Custom Computer Programming Services",
-        in_occupation_jobs: 1388,
-        jobs: 15252
-      },
-      {
-        naics: "541512",
-        title: "Aircraft Manufacturing",
-        in_occupation_jobs: 444,
-        jobs: 71612
-      },
-      {
-        naics: "541512",
-        title: "Other Computer Related Services",
-        in_occupation_jobs: 296,
-        jobs: 3245
-      }
-    ]
-  }
-};
+
+      axios.get('https://run.mocky.io/v3/a2cc3707-8691-4188-8413-6183a7bb3d32').then((response) => {
+        //Set Component Data
         this.occupation = response.data.occupation.title;
         this.region = response.data.region.title;
         this.summary = response.data.summary;
@@ -149,12 +60,7 @@ export default {
         this.trendComparison = response.data.trend_comparison;
 
         this.loaded=true;
-
-      /*axios.get('https://run.mocky.io/v3/a2cc3707-8691-4188-8413-6183a7bb3d32').then((response) => {
-        //console.log(response.data);
-        //Set Component Data
-
-      });*/
+      });
     }
   },
 
